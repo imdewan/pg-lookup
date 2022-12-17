6f2f8968e3d44b16a1d31998bdbddfe6c58ce526
MrDSA04 <mydsaproduction@gmail.com>
2022-12-17 11:45:20 +0530

    NEW

--- client/src/screens/login.js
+++ client/src/screens/login.js
@@ -9,6 +9,7 @@ return (
 <div style={{width:"100vw",height:"100vh",background:"#000f29", display: 'flex',
     alignItems: 'center',
     justifyContent: 'center'}}>  
+
     <ReactCardFlip isFlipped={flip} 
             flipDirection="horizontal"> 
     {/* Login Form */}
@@ -19,6 +20,7 @@ return (
     padding:"20px",
     boxShadow: "0px 0px 24px 0px rgba(255,255,255,1)",
     borderRadius:"10px",
+    maxWidth:"100vw",maxHeight:"100vh",
     }}>
         <center>
         <img 
@@ -64,7 +66,8 @@ return (
     margin:"auto",
     padding:"20px",
     boxShadow: "0px 0px 24px 0px rgba(255,255,255,1)",
-    borderRadius:"10px"
+    borderRadius:"10px",
+    maxWidth:"100vw",maxHeight:"100vh",
     }}>
         <center>
         <img 
@@ -76,37 +79,41 @@ return (
               style={{borderRadius:"50%", margin:"5px"}}
             />
         </center>
-        <Form>
+        <Form method="post" action="http://localhost:9000/register">
         <Form.Group className="mb-3" controlId="formBasicName">
             <Form.Label>Your Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter Name" />
+            <Form.Control type="text" name="name" placeholder="Enter Name" required />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control type="email" name="email" placeholder="Enter email" required />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control type="password" name="password" placeholder="Password" required />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicAge">
             <Form.Label>Age</Form.Label>
-            <Form.Control type="number" placeholder="Enter Age" />
+            <Form.Control type="number" name="age" placeholder="Enter Age" required />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicAge">
+            <Form.Label>Phone/Mobile</Form.Label>
+            <div>
+            <strong style={{width:"10%"}}>+91</strong>
+            <Form.Control style={{width:"90%"}} type="number" name="phone" placeholder="Enter Phone number" required />
+            </div>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicAddress">
             <Form.Label>Gender</Form.Label>
-            <Form.Select aria-label="Default select">
+            <Form.Select name="gender" aria-label="Default select">
                 <option>Select Gender</option>
                 <option value="1">Male</option>
                 <option value="2">Female</option>
                 <option value="3">Others</option>
             </Form.Select>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicAddress">
-            <Form.Label>Permanent Address</Form.Label>
-            <Form.Control type="text" placeholder="Permanent Address" />
-        </Form.Group>
+        
         <Button variant="primary" style={{width:"100%"}} type="submit">
             Submit
         </Button>
--- server/index.js
+++ server/index.js
@@ -1,15 +1,30 @@
+var crypto = require('crypto');
 const express = require('express');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var mysql = require('mysql');
+var nodemailer = require('nodemailer');
 const app = express();
 const port = 9000;
 
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: ""
+    password: "",
+    database: "pg-lookup"
   });
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'bunch.of.devs@gmail.com',
+      pass: 'devs@98765'
+    }
+});
+
+app.use(express.urlencoded({
+    extended: true
+}))
 
 app.get('/', (req, res) => {
   res.send('None!');
@@ -30,20 +45,61 @@ app.get('/session', function(req, res){
    }
 });
 app.get('/login', function(req, res){
-
+    
  });
- app.get('/register', function(req, res){
+ app.get('/auth', function(req, res){
+    con.query("SELECT * FROM users WHERE email = '"+req.query.u+"' AND activation = '"+req.query.a+"'", function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        if (typeof result !== 'undefined' && result > 0) {
+            var sql = "UPDATE users SET activation = 'activated' WHERE email = '"+req.query.u+"'";
+            con.query(sql, function (err, result) {
+                if (err) throw err;
+                console.log(result.affectedRows + " record(s) updated");
+            });
+        }
+      });
+ });
+ app.post('/register', function(req, res){
     con.connect(function(err) {
         if (err) throw err;
-        console.log("Connected!");
-        var sql = "INSERT INTO users (name, email, password, gender, age, address) VALUES ('Company Inc', 'Highway 37')";
-        con.query(sql, function (err, result) {
-          if (err) throw err;
-          console.log("1 record inserted");
-        });
+        console.log(req.body.name);
+        con.query("SELECT * FROM users WHERE email = '"+req.query.u+"' AND activation = '"+req.query.a+"'", function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            if (typeof result === 'undefined' || result == 0) {
+                var hashedpwd = sha256(req.body.password);
+                var activation = crypto.randomBytes(16).toString("hex");
+                var sql = "INSERT INTO users (name, email, password, gender, age, address, phone, activation) VALUES ('"+req.body.name+"','"+req.body.email+"','"+hashedpwd+"','"+req.body.gender+"','"+req.body.age+"','"+req.body.address+"','"+req.body.phone+",'"+activation+"')";
+                con.query(sql, function (err, result) {
+                if (err) throw err;
+                console.log("1 record inserted");
+                var mailOptions = {
+                    from: 'bunch.of.devs@gmail.com',
+                    to: req.body.email,
+                    subject: 'Verification for PG-LookUp',
+                    text: ' Click this link to verify- http://localhost:9000/auth/?a='+activation+'&u='+req.body.email
+                };
+                transporter.sendMail(mailOptions, function(error, info){
+                    if (error) {
+                    console.log(error);
+                    } else {
+                    console.log('Email sent: ' + info.response);
+                    }
+                });
+                });
+            }else{
+                res.send("Already Registered");
+            }
+          });
+        
       });
  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
+
+function sha256(content) {  
+    return crypto.createHash('sha3-256').update(content).digest('hex')
+  }
