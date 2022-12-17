a82a09372688440b62f17d2ea13c3d561c747220
MrDSA04 <mydsaproduction@gmail.com>
2022-12-17 20:26:23 +0530

    added auth and map

--- client/src/screens/home.js
+++ client/src/screens/home.js
@@ -1,12 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import NavBarHead from '../nav';
+import useState from "react";
+import { withScriptjs, InfoWindow, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+
+
 const Home = () => {
+
 return (
 	<div>
     <NavBarHead />
-	<h1>Welcome!</h1>
+    <ReportsPage />
 	</div>
 );
 };
+
+const handleActiveMarker = (marker) => {
+    console.log(marker);
+  };
+
+const Map = withScriptjs(
+    withGoogleMap(props => (
+        <GoogleMap
+            defaultZoom={12}
+            defaultCenter={{ lat: 12.840994, lng: 77.516565 }}
+            
+        >
+            {props.marks.map((mark, index) => (
+                
+                <Marker
+                    key={index}
+                    position={mark}
+                    onClick={() => handleActiveMarker(mark)}    
+                >
+                
+                    <InfoWindow >
+                      <div>{props.names[index]}</div>
+                    </InfoWindow>
+                  
+                  </Marker>
+            ))}
+        </GoogleMap>
+    ))
+);
+
+class ReportsPage extends Component {
+    state = {
+        marks: [{ lat: 12.840994, lng: 77.516565 }
+  , { lat: 12.849488, lng: 77.521457 }
+  ],
+  names:["ok1","bk2"]
+  
+    };
+
+   
+
+    render() {
+        const { marks,names } = this.state;
+        return (
+            <div>
+                
+                <Map
+                    googleMapURL="http://maps.googleapis.com/maps/api/js?key=AIzaSyBzwH4aiQxMFuHcUcBHEaYZ4SAxCPm7Xhg"
+                    loadingElement={<div style={{ height: `100%` }} />}
+                    containerElement={<div style={{ height: `90vh` }} />}
+                    mapElement={<div style={{ height: `100%` }} />}
+                    onClick={handleActiveMarker(marks)}
+                    marks={marks}
+                    names={names}
+                />;
+            </div>
+        );
+    }
+}
+
 
 export default Home;
